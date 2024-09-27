7b8d8d573ead70ac55c14a88a5c458c7f8a5bdb8
Amr Mosallem <amromsllam@gmail.com>
2024-09-27 06:36:40 +0300

    Add Shared Header & Search Accessibility From Other Pages

--- scripts/shared/utilities.js
+++ scripts/shared/utilities.js
@@ -6,3 +6,26 @@ function searchProducts(query) {
     if (!query) return;
     window.location.href = "amazon.html?search=" + query;
 }
+
+function addHeader(){
+    let header = `    <header>
+        <p class="icon">
+            ./amazon
+        </p>
+        <div class="search">
+            <input type="text" placeholder="Search" class="search-bar">
+            <i class='bx bx-search'></i>
+        </div>
+        <div class="features">
+            <div class="cart">
+                <i class='bx bx-shopping-bag'></i>
+                <p>Cart <span id="counter">0</span></p>
+            </div>
+            <div class="order">
+                <p>Returns & Orders</p>
+            </div>
+        </div>
+        </div>
+    </header>`;
+    document.body.innerHTML = header + document.body.innerHTML;
+}
